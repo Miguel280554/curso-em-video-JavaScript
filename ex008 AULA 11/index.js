c1374efbc74002870dd5afa2274e045e0bd6ad00
Miguel280554 <miguel.prudenciano1@gmail.com>
2025-03-12 14:51:10 -0300

    curso JS

---
+++ ex008 AULA 11/index.js
@@ -0,0 +1,5 @@
+var velocidade = 12
+if (velocidade > 70 ){
+    console.log('você ultrapassou o limite de velocidade multado')
+}
+console.log('ande com cintos de segurança!!!')
---
+++ ex009 AULA 11/index.js
@@ -0,0 +1,7 @@
+var pais = 'brazil'
+console.log(`você mora no/a ${pais}`)
+if(pais != 'EUA'){
+    console.log('você é estrageiro!')
+} else{
+    console.log('você é americano!')
+}

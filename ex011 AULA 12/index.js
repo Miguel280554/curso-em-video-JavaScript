56475e65f04c2e49d5d16c240cddf60cacb4c7fc
Miguel280554 <miguel.prudenciano1@gmail.com>
2025-03-24 17:10:11 -0300

    Curso JS

---
+++ ex011 AULA 12/index.js
@@ -0,0 +1,8 @@
+var number = 18
+if( 18 <= number <= 16 || number >= 67){
+    console.log('voto opicional')
+} else if(number >= 18){
+    console.log('voto obrigatorio')
+} else if(number < 16){
+    console.log('não vota')
+}
---
+++ teste004/script.js
@@ -0,0 +1,6 @@
+const agora = new Date();
+const hora = agora.getHours();
+const minutos = agora.getMinutes();
+const segundos = agora.getSeconds();
+
+console.log(`Hora atual: ${hora}:${minutos}:${segundos}`);
